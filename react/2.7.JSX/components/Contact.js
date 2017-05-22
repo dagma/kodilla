8db984539c688ccfb2e90a25d211fda3c8f95249
Dagmara Turoń <aida7@interia.pl>
2017-05-23 01:02:55 +0200

    przeksztalcenie na jsx

--- react/2.7.JSX/components/Contact.js
+++ react/2.7.JSX/components/Contact.js
@@ -1,18 +1,16 @@
-var Contact = React.createClass({
+Contact = React.createClass({
 	propTypes: {
 		item: React.PropTypes.object.isRequired,
 	},
 	render: function() {
 		return (
-		React.createElement('div', {className: 'contactItem'},
-			React.createElement('img', {
-				className: 'contactImage',
-				src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-			}),
-			React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-			React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-			React.createElement('a', {className: 'contactEmail', href: 'mailto:'+this.props.item.email}, this.props.item.email)
-			)
-		)
-	}
+			<div class='contactItem'>
+				<img class="contactImage" src="http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico"  />
+				<p class="contactLabel">Imię {this.props.item.firstName}</p>
+				<p class="contactLabel">Nazwisko {this.props.item.lastName}</p>
+				<a href="{this.props.item.email}" class="contactEmail">Imię {this.props.item.email}</a>
+			</div>
+
+		);
+	},
 });
