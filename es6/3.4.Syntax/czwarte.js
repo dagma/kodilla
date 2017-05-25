58e71016bfbc4a040bd14827b9e92d1dba1ba4b4
Dagmara Turoń <aida7@interia.pl>
2017-05-25 22:48:19 +0200

    fix

--- es6/3.4.Syntax/czwarte.js
+++ es6/3.4.Syntax/czwarte.js
@@ -1,4 +1,17 @@
-const params = [1, 4, 'Iwona', false, 'Nowak'];
-const [, , firstName, ,lastName] = params;
+const average = (...args) => {
+	let wynik = 0;
+	if (args.length !== 0) {
+		let suma = 0;
+		args.forEach(function(arg) {
+			suma = suma + arg;
+		});
+		wynik = suma / args.length;
+	}
+	return wynik;
+};
 
-console.log(firstName, lastName);
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const [...numbers] = grades;
+
+console.log(average(...numbers));
