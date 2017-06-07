9e8e65eb5429b5de4cda79464535f66983080f27
Dagmara Turoń <aida7@interia.pl>
2017-06-07 22:01:38 +0200

    remove spaces

--- webpack/4.4.ToDo/src/components/TodoForm.js
+++ webpack/4.4.ToDo/src/components/TodoForm.js
@@ -3,17 +3,15 @@ const TodoForm = props => {
 	return (
 		<div>
 			<input
-				type = "text" 
-				placeholder = "napisz zadanie"
-				value = {props.inputValue} 
+				type="text" 
+				placeholder="napisz zadanie"
+				value={props.inputValue} 
 				onChange={(e) => {
 					props.changeInputValue(e.target.value);
 				}}
 			/>
 			<button 
-				onClick={() => {
-					props.addTodo(props.inputValue);
-				}}
+				onClick={() => { props.addTodo(props.inputValue); }}
 			>
 				dodaj
 			</button>
