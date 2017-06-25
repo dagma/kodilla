e29b345c59807599d53c555af52e1d6bc37de41e
Dagmara Turoń <aida7@interia.pl>
2017-06-26 00:14:34 +0200

    google

---
+++ express/5.6.Logowanie/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': ' 379992897708-5lejia2uh7473ah64b5f76lbv40dprai.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'uBTfRCj2kiD1Y81vq-ycvwJZ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+// 299137982763-rhbkvt0la7udb4aio20nhgdotdhgh6o2.apps.googleusercontent.com
+// NU9OWXYnQQ4SBcIT3E-5qLdq
--- express/5.6.Logowanie/server.js
+++ express/5.6.Logowanie/server.js
@@ -1,12 +1,59 @@
+
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+passport.use(new GoogleStrategy({ 
+	clientID: config.GOOGLE_CLIENT_ID, 
+	clientSecret:config.GOOGLE_CLIENT_SECRET, 
+	callbackURL: config.CALLBACK_URL 
+	}, 
+	function(accessToken, refreshToken, profile, cb) { 
+		googleProfile = { 
+			id: profile.id, 
+			displayName: profile.displayName }; 
+		cb(null, profile); 
+	} 
+));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/view-login', function(req, res) {
-	res.render('view-login');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app routes
+app.get('/', function(req, res) {
+	res.render('index', {user: req.user});
 });
+app.get('/logged', function(req, res) {
+	res.render('logged', {user: googleProfile});
+});
+
+// passport routes
+app.get('/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+app.get('/auth/google/callback',
+	passport.authenticate('google', {
+		successRedirect : '/logged',
+		failureRedirect: '/'
+	})
+);
+
 app.get('/auth/google', function(req, res) {
 	res.render('view-start');
 });
@@ -15,3 +62,5 @@ app.listen(3000);
 app.use(function(req, res, next) {
 	res.status(404).send('Wybacz, nie moglismy odnaleźć tego, czego żądasz!');
 });
+
+
