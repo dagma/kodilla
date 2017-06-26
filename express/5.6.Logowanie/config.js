ced0a44aab99007c2c8d34f1a9e34ded9ad9324d
Dagmara Turoń <aida7@interia.pl>
2017-06-26 10:31:40 +0200

    remove space

--- express/5.6.Logowanie/config.js
+++ express/5.6.Logowanie/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'GOOGLE_CLIENT_ID': ' 379992897708-5lejia2uh7473ah64b5f76lbv40dprai.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_ID': '379992897708-5lejia2uh7473ah64b5f76lbv40dprai.apps.googleusercontent.com',
 	'GOOGLE_CLIENT_SECRET': 'uBTfRCj2kiD1Y81vq-ycvwJZ',
 	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
