2f6cbf93995a874332c07bc0f14586c7a247bea8
Dagmara Turoń <aida7@interia.pl>
2017-07-02 21:33:00 +0200

    fix

--- express/5.8.Projekt2/routes/profile.js
+++ express/5.8.Projekt2/routes/profile.js
@@ -7,11 +7,7 @@ var forms = require('forms');
 
 var profileForm = forms.create({
 	givenName: forms.fields.string({ required: true }), 
-	surname: forms.fields.string({ required: true }), 
-	streetAddress: forms.fields.string(), 
-	city: forms.fields.string(), 
-	state: forms.fields.string(), 
-	zip: forms.fields.string() 
+	surname: forms.fields.string({ required: true })
 });
 
 function renderForm(req, res, locals) {
@@ -20,10 +16,6 @@ function renderForm(req, res, locals) {
 		csrfToken: req.csrfToken(), 
 		givenName: req.user.givenName, 
 		surname: req.user.surname, 
-		streetAddress: req.user.customData.streetAddress, 
-		city: req.user.customData.city, 
-		state: req.user.customData.state, 
-		zip: req.user.customData.zip
 	}, locals || {})); 
 }
 
@@ -39,14 +31,7 @@ module.exports = function profile() {
 			success: function(form) {
 				req.user.givenName = form.data.givenName; 
 				req.user.surname = form.data.surname; 
-				req.user.customData.streetAddress = form.data.streetAddress; 
-				req.user.customData.city = form.data.city; 
-				req.user.customData.state = form.data.state; 
-				req.user.customData.zip = form.data.zip; 
-
-				
-				req.user.save(); 
-				console.log(req.user.customData);
+				//req.user.customData.save();
 				req.user.save(function(err) {
 					if (err) {
 						if (err.developerMessage){
