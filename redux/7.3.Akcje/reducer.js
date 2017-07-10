602e35fb63a06c00fcdd5d15aa1aa05dd459e5bb
Dagmara Turoń <aida7@interia.pl>
2017-07-10 14:11:52 +0200

    combineReducers

--- redux/7.3.Akcje/reducer.js
+++ redux/7.3.Akcje/reducer.js
@@ -1,60 +1,20 @@
-import {ADD_COMMENT, EDIT_COMMENT, REMOVE_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions';
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
 const initialState = {
 	comments: [],
 	users: []
 };
 
-/*
-function reducer(state, action) {
-	if (!state) {
-		return initialState;
-	}
-	return state;
-}
-*/
-// Składnia ES6
-function reducer(state = initialState, action) {
-	switch(action.type) {
-		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [
-				{
-					id: action.id,
-					text: action.text,
-					votes: 0
-				}
-				, ...state]
-			});
-		case REMOVE_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id !== action.id)
-			});
-		case EDIT_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => {
-					if (comment.id === action.id) {
-						comment.text = action.text;
-					}
-				});
-			});
-		case THUMB_UP_COMMENT: 
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => {
-					if (comment.id === action.id) {
-						comment.votes++;
-					}
-				});
-			});
-		case THUMB_DOWN_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => {
-					if (comment.id === action.id) {
-						comment.votes--;
-					}
-				});
-			});
-		default: 
-			return state;
-	}
+const app = combineReducers({
+	comments,
+	users
+});
+
+function app(state = initialState, action) {
+	return {
+		comments: comments(state.comments, action),
+		users: users(state.users, action)
+	};
 }
