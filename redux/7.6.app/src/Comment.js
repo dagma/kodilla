0dce8df4ff1a14cdeae237e5eaa27f1aebdbeb13
Dagmara Turoń <aida7@interia.pl>
2017-07-12 11:02:11 +0200

    fix

--- redux/7.6.app/src/Comment.js
+++ redux/7.6.app/src/Comment.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const Comment = ({text, votes}) => <li>{text} <span>votes: {votes}</span></li>;
+const Comment = ({text, votes, id, thumbUpComment}) => 
+	<li>
+		{text} <span>votes: {votes}</span> <button onCLick={() => thumbUpComment(id)}>Thumb up</button>
+	</li>;
 
 export default Comment;
--- redux/7.6.app/src/actions.js
+++ redux/7.6.app/src/actions.js
@@ -1,19 +1,30 @@
 // Actions type
 export const ADD_COMMENT = 'ADD_COMMENT';
-export const EDIT_COMMENT = 'EDIT_COMMENT';
-export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+/*
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+*/
 var uuid = require('uuid-v4');
 
 // Action creators
-function addComment(text) {
+export function addComment(text) {
     return {
         type: ADD_COMMENT,
         text: text,
-        id: uuid.v4()
+        id: uuid()
     };
 }
+
+export function thumbUpComment(commentId) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id: commentId
+	};
+}
+
+
 /*
 function editComment(id, text) {
 	return {
@@ -30,14 +41,6 @@ function removeComment(id) {
 	};
 }
 
-function thumbUpComment(id, vote) {
-	return {
-		type: THUMB_UP_COMMENT,
-		id: id,
-		vote: vote
-	};
-}
-
 function thumbDownComment(id, vote) {
 	return {
 		type: THUMB_DOWN_COMMENT,
@@ -45,12 +48,4 @@ function thumbDownComment(id, vote) {
 		vote: vote
 	};
 }
-
-
-export default removeComment;
-export default editComment;
-export default thumbDownComment;
-export default thumbUpComment;
 */
-
-export default addComment;
